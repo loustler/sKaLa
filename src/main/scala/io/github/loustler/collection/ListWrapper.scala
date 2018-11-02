9040eeabfc84494d0414fe13f45992b7d406c647
loustler <dev.loustler@gmail.com>
2018-11-02 20:06:12 +0900

    Updated project settings
    - Added scalafmt as default
    - Added scalastyle as default
    - Changed build sbt to simple

---
+++ src/main/scala/io/github/loustler/collection/ListWrapper.scala
@@ -0,0 +1,19 @@
+package io.github.loustler.collection
+
+/**
+  * @author loustler
+  * @since 10/27/2018
+  */
+object ListWrapper {
+  def prepend[A](list: List[A], x: A): List[A] = x :: list
+
+  def prepend[A](list: List[A], x: A*): List[A] = x.foldLeft(list)(prepend)
+
+  def append[A](list: List[A], x: A): List[A] = list :+ x
+
+  def append[A](list: List[A], x: A*): List[A] = merge(list, x.toList)
+
+  def merge[A](x: List[A], y: List[A]): List[A] = x ::: y
+
+  def merge[A](x: List[A]*): List[A] = x.foldLeft(List[A]())(merge)
+}
